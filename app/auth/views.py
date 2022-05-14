09e57b6bfc1caeb97512d07032fb4005be731441
Kisavi <deniskisavi@gmail.com>
2022-05-14 16:58:39 +0300

    create validation messages for user

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,35 @@
 from . import auth
-from flask import render_template
+from flask import render_template, request, flash
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(username) < 2:
+            flash('Pick a username that is greater than 2 characters.', category='error')
+        elif len(password1) < 7:
+            flash('Use a stronger password that is more than 7 characters long.', category='error')
+        elif password1 != password2:
+            flash('Passwords did not match!!!', category='error')
+        else:
+            # create a new user if form is valid
+            flash('Account created successfully.', category='success')
+
     return render_template('register.html')
 
 
-@auth.route('/register')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "Login"
+    data = request.form
+    print(data)
+    return render_template('register.html')
 
 
 @auth.route('/logout')
